9df5ea41780e32b3c24ef0001862ac8127dd34f0
Ahmed111222333 <68826015+Ahmed111222333@users.noreply.github.com>
2021-01-25 13:55:19 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function addRemoveClass() {
+  var addclass = document.getElementById("ac");
+  var removeclass = document.getElementById("rc");
+  addclass.classList.add("active");
+  removeclass.classList.remove("active");
+}
+
+// $("#arc").click(function(){
+//     $("#ac").addClass("active");
+//     $("#rc").removeClass("active");
+//   });
+  
