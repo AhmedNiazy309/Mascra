04eed131519a8731c6f2855f58250ee16d47fa27
Ahmed111222333 <68826015+Ahmed111222333@users.noreply.github.com>
2021-02-03 14:56:15 +0200

    3/2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,46 @@
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+
+  var $this = $(this),
+    label = $this.prev('label');
+
+  if (e.type === 'keyup') {
+    if ($this.val() === '') {
+      label.removeClass('active highlight');
+    } else {
+      label.addClass('active highlight');
+    }
+  } else if (e.type === 'blur') {
+    if ($this.val() === '') {
+      label.removeClass('active highlight');
+    } else {
+      label.removeClass('highlight');
+    }
+  } else if (e.type === 'focus') {
+
+    if ($this.val() === '') {
+      label.removeClass('highlight');
+    }
+    else if ($this.val() !== '') {
+      label.addClass('highlight');
+    }
+  }
+
+});
+
+$('.tab a').on('click', function (e) {
+
+  e.preventDefault();
+
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+
+  $(target).fadeIn(600);
+
+});
 var addclass = document.getElementById("ac");
 var removeclass = document.getElementById("rc");
 var addclass2 = document.getElementById("pills-current");
@@ -7,9 +50,14 @@ function addRemoveClass() {
   // active show
   addclass.classList.add("active");
   removeclass.classList.remove("active");
+  //   addclass2.classList.add("active show");
+  // removeclass2.classList.remove("active show");
+}
+
+
+    // active show
 //   addclass2.classList.add("active show");
 // removeclass2.classList.remove("active show");
-}
 // var addRemoveClass = document.getElementById("addRemoveClass");
 // addRemoveClass.onclick = function(){
 // //   var addclass = document.getElementById("ac");
