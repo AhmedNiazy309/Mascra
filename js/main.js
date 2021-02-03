cf63d51ebde3e2913e01d953aaf118404aec9d7f
Ahmed111222333 <68826015+Ahmed111222333@users.noreply.github.com>
2021-02-03 13:16:41 +0200

    3/2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
+var addclass = document.getElementById("ac");
+var removeclass = document.getElementById("rc");
+var addclass2 = document.getElementById("pills-current");
+var removeclass2 = document.getElementById("pills-previous");
 function addRemoveClass() {
-  var addclass = document.getElementById("ac");
-  var removeclass = document.getElementById("rc");
+  "use strict"
+  // active show
   addclass.classList.add("active");
   removeclass.classList.remove("active");
+//   addclass2.classList.add("active show");
+// removeclass2.classList.remove("active show");
 }
+// var addRemoveClass = document.getElementById("addRemoveClass");
+// addRemoveClass.onclick = function(){
+// //   var addclass = document.getElementById("ac");
+// //   var removeclass = document.getElementById("rc");
+// //   addclass.classList.add("active");
+// //   removeclass.classList.remove("active");
+
+// }
 
 // $("#arc").click(function(){
 //     $("#ac").addClass("active");
