d18fad2e8a087d8911bd98cf6494a6e25547b2c6
Ahmed111222333 <68826015+Ahmed111222333@users.noreply.github.com>
2021-03-27 18:46:30 +0200

    27/3

--- js/main.js
+++ js/main.js
@@ -1,44 +1,73 @@
-$('.form').find('input, textarea').on('keyup blur focus', function (e) {
-
-  var $this = $(this),
-    label = $this.prev('label');
-
-  if (e.type === 'keyup') {
-    if ($this.val() === '') {
-      label.removeClass('active highlight');
-    } else {
-      label.addClass('active highlight');
+$(document).ready(function () {
+  $(".client-carousel").owlCarousel({
+    item: 1,
+    rtl: true,
+    stagePadding: 0,
+    responsiveClass: true,
+    center: true,
+    margin: 0,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+        loop: true
+      },
+      600: {
+        items: 2,
+        nav: false,
+        loop: true
+      },
+      1000: {
+        items: 4,
+        nav: true,
+        loop: true,
+        margin: 20
+      }
     }
-  } else if (e.type === 'blur') {
-    if ($this.val() === '') {
-      label.removeClass('active highlight');
-    } else {
-      label.removeClass('highlight');
+
+  });
+  $('.form').find('input, textarea').on('keyup blur focus', function (e) {
+
+    var $this = $(this),
+      label = $this.prev('label');
+
+    if (e.type === 'keyup') {
+      if ($this.val() === '') {
+        label.removeClass('active highlight');
+      } else {
+        label.addClass('active highlight');
+      }
+    } else if (e.type === 'blur') {
+      if ($this.val() === '') {
+        label.removeClass('active highlight');
+      } else {
+        label.removeClass('highlight');
+      }
+    } else if (e.type === 'focus') {
+
+      if ($this.val() === '') {
+        label.removeClass('highlight');
+      } else if ($this.val() !== '') {
+        label.addClass('highlight');
+      }
     }
-  } else if (e.type === 'focus') {
 
-    if ($this.val() === '') {
-      label.removeClass('highlight');
-    } else if ($this.val() !== '') {
-      label.addClass('highlight');
-    }
-  }
+  });
 
-});
+  $('.tab a').on('click', function (e) {
 
-$('.tab a').on('click', function (e) {
+    e.preventDefault();
 
-  e.preventDefault();
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
 
-  $(this).parent().addClass('active');
-  $(this).parent().siblings().removeClass('active');
+    target = $(this).attr('href');
 
-  target = $(this).attr('href');
+    $('.tab-content > div').not(target).hide();
 
-  $('.tab-content > div').not(target).hide();
+    $(target).fadeIn(600);
 
-  $(target).fadeIn(600);
-
+  });
 });
 var addclass = document.getElementById("ac");
 var removeclass = document.getElementById("rc");
@@ -50,21 +79,3 @@ function addRemoveClass() {
   addclass.classList.add("active");
   removeclass.classList.remove("active");
 }
-
-
-// active show
-//   addclass2.classList.add("active show");
-// removeclass2.classList.remove("active show");
-// var addRemoveClass = document.getElementById("addRemoveClass");
-// addRemoveClass.onclick = function(){
-// //   var addclass = document.getElementById("ac");
-// //   var removeclass = document.getElementById("rc");
-// //   addclass.classList.add("active");
-// //   removeclass.classList.remove("active");
-
-// }
-
-// $("#arc").click(function(){
-//     $("#ac").addClass("active");
-//     $("#rc").removeClass("active");
-//   });
